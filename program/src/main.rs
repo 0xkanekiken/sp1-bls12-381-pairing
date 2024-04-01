183a81f560bac0e2af0de77468dd35a7b3807015
0xkanekiken <100861945+0xKanekiKen@users.noreply.github.com>
2024-04-01 17:52:28 +0000

    feat: call bls12381 pairing without any precompile
    Signed-off-by: 0xkanekiken <100861945+0xKanekiKen@users.noreply.github.com>

--- program/src/main.rs
+++ program/src/main.rs
@@ -3,17 +3,36 @@
 #![no_main]
 sp1_zkvm::entrypoint!(main);
 
-pub fn main() {
-    // let n = sp1_zkvm::io::read::<u32>();
-    // let mut a = 0;
-    // let mut b = 1;
-    // let mut sum;
-    // for _ in 1..n {
-    //     sum = a + b;
-    //     a = b;
-    //     b = sum;
-    // }
+extern crate snowbridge_amcl;
 
-    // sp1_zkvm::io::write(&a);
-    // sp1_zkvm::io::write(&b);
+pub use self::snowbridge_amcl::bls381 as BLSCurve;
+
+use snowbridge_amcl::bls381::bls381::utils::{serialize_uncompressed_g1, serialize_uncompressed_g2};
+use BLSCurve::ecp::ECP;
+use BLSCurve::ecp2::ECP2;
+use BLSCurve::pair;
+
+#[sp1_derive::cycle_tracker]
+pub fn main() {
+    pairing_check();
+}
+
+fn pairing_check() -> bool {
+    let g1_point = ECP::generator();
+    let g2_point = ECP2::generator();
+    let mut r = pair::initmp();
+
+    println!("cycle-tracker-start: pair_another_start");
+    pair::another(&mut r, &g2_point, &g1_point);
+    println!("cycle-tracker-start: pair_another_end");
+
+    println!("cycle-tracker-start: miller_start");
+    let mut v = pair::miller(&r);
+    println!("cycle-tracker-start: miller_end");
+
+    println!("cycle-tracker-start: fexp_start");
+    v = pair::fexp(&v);
+    println!("cycle-tracker-start: fexp_end");
+
+    v.is_unity()
 }
--- script/src/main.rs
+++ script/src/main.rs
@@ -1,21 +1,14 @@
 //! A simple script to generate and verify the proof of a given program.
 
-use sp1_core::{SP1Prover, SP1Stdin, SP1Verifier};
+use sp1_core::{SP1Prover, SP1Stdin, SP1Verifier, utils};
 
 const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
 
 fn main() {
     // Generate proof.
-    let mut stdin = SP1Stdin::new();
-    let n = 500u32;
-    stdin.write(&n);
-    let mut proof = SP1Prover::prove(ELF, stdin).expect("proving failed");
-
-    // Read output.
-    let a = proof.stdout.read::<u32>();
-    let b = proof.stdout.read::<u32>();
-    println!("a: {}", a);
-    println!("b: {}", b);
+    utils::setup_logger();
+    let stdin = SP1Stdin::new();
+    let proof = SP1Prover::prove(ELF, stdin).expect("proving failed");
 
     // Verify proof.
     SP1Verifier::verify(ELF, &proof).expect("verification failed");
