e7295a07ff0ef59c4893684d6343c39fb2eebfed
0xkanekiken <100861945+0xKanekiKen@users.noreply.github.com>
2024-04-03 04:44:47 +0000

    pairing_example
    Signed-off-by: 0xkanekiken <100861945+0xKanekiKen@users.noreply.github.com>

--- program/src/main.rs
+++ program/src/main.rs
@@ -5,12 +5,18 @@ sp1_zkvm::entrypoint!(main);
 
 extern crate snowbridge_amcl;
 
-pub use self::snowbridge_amcl::bls381 as BLSCurve;
+pub use snowbridge_amcl::bls381 as BLSCurve;
+use snowbridge_amcl::bls381::rom;
 
-use snowbridge_amcl::bls381::bls381::utils::{serialize_uncompressed_g1, serialize_uncompressed_g2};
+use crate::BLSCurve::pair::fexp;
+use snowbridge_amcl::bls381::bls381::utils::{
+    serialize_uncompressed_g1, serialize_uncompressed_g2,
+};
+use snowbridge_amcl::rand::RAND;
+use BLSCurve::big::Big;
 use BLSCurve::ecp::ECP;
 use BLSCurve::ecp2::ECP2;
-use BLSCurve::pair;
+use BLSCurve::pair::{ate, g1mul, g2mul, gtpow};
 
 #[sp1_derive::cycle_tracker]
 pub fn main() {
@@ -18,21 +24,78 @@ pub fn main() {
 }
 
 fn pairing_check() -> bool {
-    let g1_point = ECP::generator();
-    let g2_point = ECP2::generator();
-    let mut r = pair::initmp();
+    let mut rng = create_rng();
 
-    println!("cycle-tracker-start: pair_another_start");
-    pair::another(&mut r, &g2_point, &g1_point);
-    println!("cycle-tracker-start: pair_another_end");
+    // Generate random points in G1 and G2
+    let P = ECP::generator();
+    let Q = ECP2::generator();
 
-    println!("cycle-tracker-start: miller_start");
-    let mut v = pair::miller(&r);
-    println!("cycle-tracker-start: miller_end");
+    // Generate random scalars
+    let a = Big::randomnum(&Big::new_ints(&rom::CURVE_ORDER), &mut rng);
+    let b = Big::randomnum(&Big::new_ints(&rom::CURVE_ORDER), &mut rng);
 
-    println!("cycle-tracker-start: fexp_start");
-    v = pair::fexp(&v);
-    println!("cycle-tracker-start: fexp_end");
+    // Multiply points by scalars
+    println!("cycle-tracker-start: g1_mul");
+    let P_a = g1mul(&P, &a); // aP in G1
+    println!("cycle-tracker-start: g1_mul");
 
-    v.is_unity()
+    println!("cycle-tracker-start: g2_mul");
+    let Q_b = g2mul(&Q, &b); // bQ in G2
+    println!("cycle-tracker-start: g2_mul");
+
+    // Step 3: Compute pairings
+    println!("cycle-tracker-start: ate_Q_Pa");
+    let e1 = ate(&Q, &P_a); // e(aP, Q)
+    println!("cycle-tracker-start: ate_Q_Pa");
+
+    println!("cycle-tracker-start: ate_P_bQ");
+    let e2 = ate(&Q_b, &P); // e(P, bQ)
+    println!("cycle-tracker-start: ate_P_bQ");
+
+    println!("cycle-tracker-start: ate_P_Q");
+    let e3 = ate(&Q, &P); // e(P, Q)
+    println!("cycle-tracker-start: ate_P_Q");
+
+    println!("cycle-tracker-start: gtpow");
+    let e3_ab = gtpow(&e3, &(Big::smul(&a, &b))); // (e(P, Q))^(ab)
+    println!("cycle-tracker-start: gtpow");
+
+    // Step 4: Check bilinearity property
+    fexp(&e1).eq(&fexp(&e2)) && fexp(&e1).eq(&fexp(&e3_ab))
+
+    // let mut g1_point = ECP::generator();
+
+    // let g2_point = ECP::generator();
+    // let multiplier: [u8; 48] = [
+    //     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    //     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+    // ];
+    // let multiplier = Big::from_byte_array(&multiplier, 0);
+
+    // println!("cycle-tracker-start: bls12381_mul");
+    // let g2_point = g2_point.mul(&multiplier);
+    // println!("cycle-tracker-start: bls12381_mul");
+
+    // // println!("g2_point: {:?}", serialize_uncompressed_g1(&g2_point));
+
+    // println!("cycle-tracker-start: bls12381_add");
+    // g1_point.add(&g2_point);
+    // println!("cycle-tracker-start: bls12381_add");
+
+    // // println!("g1_point: {:?}", serialize_uncompressed_g1(&g1_point));
+
+    // g2_point
+}
+
+fn create_rng() -> RAND {
+    let mut raw: [u8; 100] = [0; 100];
+
+    let mut rng = RAND::new();
+    rng.clean();
+    for i in 0..100 {
+        raw[i] = i as u8
+    }
+
+    rng.seed(100, &raw);
+    rng
 }
--- script/src/main.rs
+++ script/src/main.rs
@@ -1,22 +1,20 @@
-//! A simple script to generate and verify the proof of a given program.
+use sp1_core::{utils, SP1Prover, SP1Stdin, SP1Verifier};
 
-use sp1_core::{SP1Prover, SP1Stdin, SP1Verifier, utils};
-
-const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
+const BLS_PAIRING: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
 
 fn main() {
     // Generate proof.
     utils::setup_logger();
     let stdin = SP1Stdin::new();
-    let proof = SP1Prover::prove(ELF, stdin).expect("proving failed");
+    let proof = SP1Prover::prove(BLS_PAIRING, stdin).expect("proving failed");
 
     // Verify proof.
-    SP1Verifier::verify(ELF, &proof).expect("verification failed");
+    SP1Verifier::verify(BLS_PAIRING, &proof).expect("verification failed");
 
     // Save proof.
     proof
-        .save("proof-with-io.json")
+        .save("proof-with-pis.json")
         .expect("saving proof failed");
 
-    println!("succesfully generated and verified proof for the program!")
+    println!("successfully generated and verified proof for the program!")
 }
